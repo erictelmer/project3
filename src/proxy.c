c57995a299f6db538e53af2ef89fcd036a50660d
Eric Telmer <erictelmer@gmail.com>
2013-12-01 17:55:46 -0500

    we're streaming\!

--- src/proxy.c
+++ src/proxy.c
@@ -34,7 +34,7 @@
 #include "log/log.h"
 //#include "generateResponse.h"
 
-#define RAND_PORT 8088
+#define RAND_PORT 8090
 #define SERV_PORT 8080
 #define BUF_SIZE 4096
 
@@ -89,18 +89,20 @@ int close_socket(int sock)
 
 
 
-int waitForAction(fd_set master, fd_set * read_fds, int fdmax, struct timeval tv, int fdcont){
+int waitForAction(fd_set *master, fd_set * read_fds, int fdmax, struct timeval tv, int fdcont){
 
   int i;
 
-  *read_fds = master; // copy it
+  *read_fds = *master; // copy it
 
+  printf("FDSET: ");
   for(i=0; i<=fdmax; i++){
     if(FD_ISSET(i, read_fds)){
-      printf("FDISSET %d\n", i);
+      printf(", %d", i);
     }
   }
-
+  printf("\n");
+  printf("Fdmax = %d\n", fdmax);
 
   tv.tv_sec = 60;
   i = select(fdmax+1, read_fds, NULL, NULL, &tv);
@@ -129,15 +131,15 @@ int acceptBrowserServerConnectionToStream(int browserListener, fd_set * master, 
 
   int browser_sock;
   int server_sock;
+  struct sockaddr_in client_addr;
+  struct sockaddr_in server_addr;
+
   char str[50];
   addr_size = sizeof(browser_addr);
 
   printf("Attempting to accept connection\n");
 
   if(*stream == NULL){//new stream!
-    struct sockaddr_in client_addr;
-    struct sockaddr_in server_addr;
-
     memcpy(&client_addr.sin_addr, &commandLine->fake_ip, sizeof(struct in_addr));
     memcpy(&server_addr.sin_addr, &commandLine->www_ip, sizeof(struct in_addr));
     client_addr.sin_family = AF_INET;
@@ -150,17 +152,26 @@ int acceptBrowserServerConnectionToStream(int browserListener, fd_set * master, 
 
     *stream = newStream(&client_addr, &server_addr, commandLine->alpha);
   }
+  else{
+    memcpy(&client_addr, &(*stream)->client_addr, addr_size);
+    memcpy(&server_addr, &(*stream)->server_addr, addr_size);
+  }
 
   //create new socket to server//CHECK FOR ERRORS
   if ((server_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) printf("socket failed\n");
 
-  if (bind(server_sock, (struct sockaddr*)&(*stream)->client_addr, addr_size) < 0) {
+  client_addr.sin_port = htons(ntohs(client_addr.sin_port) + server_sock);
+
+  if (bind(server_sock, (struct sockaddr*)&client_addr, addr_size) < 0) {
     char tmp[80];
     inet_ntop(AF_INET, &(*stream)->client_addr.sin_addr, tmp, 80);
     printf("Bind addr:\n%s\nport:\n%d\n", tmp, ntohs((*stream)->client_addr.sin_port)); 
     printf("Bind failed\n");
+    perror("bind"); 
+    exit(EXIT_FAILURE);
+    
   }
-  
+
   if (connect(server_sock, (struct sockaddr*)&(*stream)->server_addr, addr_size)) printf("Connect failed\n");
 
   //accept new socket to browser
@@ -198,9 +209,9 @@ int acceptBrowserServerConnectionToStream(int browserListener, fd_set * master, 
 
 
 //returns bytes read
-int receive(int fd, fd_set * master, int *fdmax, int listener, char (* buf)[BUF_SIZE]){
+int receive(int fd, fd_set * master, int *fdmax, int listener, char (* buf)[BUF_SIZE], connection_list_s *connection, stream_s *stream){
   int readret;
-  int j;
+  int i;
   if ((readret = recv(fd, *buf, 1, MSG_PEEK)) > 0)//Check if connection closed
     {
       /*if (!FD_ISSET(fd, &write_fds)) {
@@ -210,27 +221,35 @@ int receive(int fd, fd_set * master, int *fdmax, int listener, char (* buf)[BUF_
 	}*/
       //Http handling//
       if ((readret = recv(fd, *buf, BUF_SIZE, 0)) > 0){
-	//	logString("Read data from fd:%d", fd);
+	//logString("Read data from fd:%d", fd);
 	return readret;
       }
       //End gttp handling//
       memset(*buf,0,BUF_SIZE);
     } 
-  if (readret == 0){//If connection closed
+ 
+  if (readret <= 0){//If connection closed
     //    logString("Connection closed on fd:%d",fd);
-    close_socket(fd);
-    FD_CLR(fd, master);
-  }
-  if (readret < 0)
-    {
-      for(j = listener + 1; j<=*fdmax; j++){
-	close_socket(j);
+    close_socket(connection->server_sock);
+    close_socket(connection->browser_sock);
+    FD_CLR(connection->server_sock, master);
+    FD_CLR(connection->browser_sock, master);
+
+    printf("Closing connection associated with sockets %d,%d\n", connection->server_sock, connection->browser_sock);
+
+    removeConnectionFromStream(connection, stream);
+    freeConnection(connection);
+
+    printf("FDSET: ");
+    for(i=0; i<=*fdmax; i++){
+      if(FD_ISSET(i, master)){
+	printf(", %d", i);
       }
-      //     logString("Error reading data from client");
-      FD_ZERO(master);
-      FD_SET(listener, master);
-      *fdmax = listener;
     }
+    printf("\n");
+
+    //change fdmax
+  }
   return -1;
 }
 
@@ -334,14 +353,15 @@ int main(int argc, char* argv[])
   while (1)
     {
       //wait for a socket to have data to read
-      sock = waitForAction(master,&read_fds, fdmax, tv, sockcont);/*blocking*/
+      printf("Sockcont = %d\n", sockcont);
+      sock = waitForAction(&master,&read_fds, fdmax, tv, sockcont);/*blocking*/
       printf("Wait for action returned, sok = %d\n", sock);
       if (sock <0) continue;//select timeout
       if (sock == 0){//read through read_fs , start from beginning
 	sockcont = 0;
 	continue;
       }
-      sockcont = sock;
+      sockcont = sock + 1;
       
       
       //Browser is requesting new connection
@@ -360,15 +380,16 @@ int main(int argc, char* argv[])
 	//Determine if coming from browser or server
 	printf("Determening if coming from browser or server\n");
 	connection =  getConnectionFromSocket(stream, sock);
-	printf("Got connections\nBrowserSock = %d, Serversock = %d\n", connection->browser_sock, connection->server_sock);
+
 	if (connection == NULL){
 	  printf("NULL connec\n");
 	  return EXIT_FAILURE;
 	}
+	printf("Got connections\nBrowserSock = %d, Serversock = %d\n", connection->browser_sock, connection->server_sock);
 
 	if (sock == connection->browser_sock){
-	  ret = receive(sock, &master, &fdmax, browserListener, &buf);
-	  printf("Recieved %d\n", ret);
+	  ret = receive(sock, &master, &fdmax, browserListener, &buf, connection, stream);
+	  printf("Recieved %d bytes from browser\n", ret);
 	  if (ret > 0)
 	    printf("Recieved from browser:\n\n\n\n%s\n\n\n\n", buf);
 	    sendResponse(connection->server_sock, buf, ret);
@@ -387,7 +408,17 @@ int main(int argc, char* argv[])
 	}
 
 	if (sock == connection->server_sock){
-	  ret = receive(sock, &master, &fdmax, browserListener, &buf);
+	  ret = receive(sock, &master, &fdmax, browserListener, &buf, connection, stream);
+	  printf("Recieved %d bytes from server\n", ret);
+	  printf("FDSET: ");
+	  int i;
+	  for(i=0; i<=fdmax; i++){
+	    if(FD_ISSET(i, &master)){
+	      printf(", %d", i);
+	    }
+	  }
+	  printf("\n");
+
 	  if (ret > 0)
 	    printf("Recieved from server:\n\n\n\n%s\n\n\n\n", buf);
 	    sendResponse(connection->browser_sock, buf, ret);
